1334d3b9fca610deb35d8877dfeaea8e3346f41b
Philip May <philip@may.la>
2023-06-06 11:55:42 +0200

    bump to 0.0.3

--- mltb2/__init__.py
+++ mltb2/__init__.py
@@ -7,4 +7,4 @@
 # Versioning follows the Semantic Versioning Specification: https://semver.org/ and
 # PEP 440 -- Version Identification and Dependency Specification:
 # https://www.python.org/dev/peps/pep-0440/
-__version__ = "0.0.3rc1"
+__version__ = "0.0.3"
