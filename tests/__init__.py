ce8cc387ffe029809c9d0addda7a2750e95f4e60
PhilipMay <philip@may.la>
2023-08-17 21:21:51 +0200

    add flake 8 copyright check

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,5 @@
+# Copyright (c) 2023 Philip May
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
 """MLTB2 test main package."""
