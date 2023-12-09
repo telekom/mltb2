1f7d803d38dd7f88d4ae6a5179165560ed2082f8
PhilipMay <philip@may.la>
2023-12-09 22:12:31 +0100

    add replace_special_whitespaces

--- mltb2/text.py
+++ mltb2/text.py
@@ -13,8 +13,17 @@ INVISIBLE_CHARACTERS: Final[Tuple[str, ...]] = (
 
 INVISIBLE_CHARACTERS_TRANS: Final[Dict[int, None]] = str.maketrans({char: None for char in INVISIBLE_CHARACTERS})
 
+SPECIAL_WHITESPACES: Final[Tuple[str, ...]] = (
+    "\u00a0",  # No-Break Space (NBSP) https://www.compart.com/en/unicode/U+00a0
+    "\u2009",  # Thin Space https://www.compart.com/en/unicode/U+2009
+    "\u202f",  # Narrow No-Break Space (NNBSP) https://www.compart.com/en/unicode/U+202f
+    "\u2007",  # Figure Space https://www.compart.com/en/unicode/U+2007
+)
 
-def remove_invisible_chars(text: str) -> str:
+SPECIAL_WHITESPACES_TRANS: Final[Dict[int, None]] = str.maketrans({char: " " for char in SPECIAL_WHITESPACES})
+
+
+def remove_invisible_characters(text: str) -> str:
     """Remove invisible characters from text.
 
     The invisible characters are defined in the constant `INVISIBLE_CHARACTERS`.
@@ -26,3 +35,16 @@ def remove_invisible_chars(text: str) -> str:
         The cleaned text.
     """
     return text.translate(INVISIBLE_CHARACTERS_TRANS)
+
+def replace_special_whitespaces(text: str) -> str:
+    """Replace special whitespaces with normal whitespaces.
+
+    The special whitespaces are defined in the constant `SPECIAL_WHITESPACES`.
+
+    Args:
+        text: The text from which the special whitespaces are to be replaced.
+
+    Returns:
+        The cleaned text.
+    """
+    return text.translate(SPECIAL_WHITESPACES_TRANS)
--- tests/test_text.py
+++ tests/test_text.py
@@ -2,16 +2,27 @@
 # This software is distributed under the terms of the MIT license
 # which is available at https://opensource.org/licenses/MIT
 
-from mltb2.text import remove_invisible_chars
+from mltb2.text import remove_invisible_characters, replace_special_whitespaces
 
 
-def test_remove_invisible_chars():
+def test_remove_invisible_characters():
     text = "Hello\u200bWorld\u00ad!"
-    result = remove_invisible_chars(text)
+    result = remove_invisible_characters(text)
     assert result == "HelloWorld!"
 
 
-def test_remove_invisible_chars_empty():
+def test_remove_invisible_characters_empty():
     text = ""
-    result = remove_invisible_chars(text)
+    result = remove_invisible_characters(text)
+    assert result == ""
+
+
+def test_replace_special_whitespaces():
+    text = "a\u00a0b\u2009c\u202fd\u2007e"
+    result = replace_special_whitespaces(text)
+    assert result == "a b c d e"
+
+def test_replace_special_whitespaces_empty():
+    text = ""
+    result = replace_special_whitespaces(text)
     assert result == ""
