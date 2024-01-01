b57c6d3fa60a639fd75891092a39c61d487389e3
Philip May <philip@may.la>
2024-01-01 19:02:00 +0100

    Add Beautiful Soup specific tools. (#126)
    * Add Beautiful Soup specific tools
    
    * improve doc
    
    * Add bs module to API reference
    
    * remove normalize_text
    
    * improve typing and lint
    
    * improve typing
    
    * Fix copyright notice regex in pyproject.toml
    
    * Add test for extract_text function
    
    * add tests
    
    * Add soup_to_md function to test_bs.py
    
    * improve typing
    
    * Add html_to_md function to test_bs.py
    
    * Refactor tests and add error handling for extract_one function
    
    * Add docstrings to extract_text, extract_one, and extract_all functions
    
    * Add docstring to remove_all function
    
    * Add remove_all function to test_bs.py
    
    * Refactor test_extract_all function in test_bs.py
    
    * Add docstrings to soup_to_md and html_to_md functions
    
    * Refactor extract_text and extract_one functions
    
    * improve doc

---
+++ mltb2/bs.py
@@ -0,0 +1,142 @@
+# Copyright (c) 2023 Philip May
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
+"""Beautiful Soup and HTML specific tools.
+
+Hint:
+    Use pip to install the necessary dependencies for this module:
+    ``pip install mltb2[bs]``
+"""
+
+from typing import Any, Dict, Optional
+
+import mdformat
+from bs4 import BeautifulSoup
+from markdownify import MarkdownConverter, markdownify
+
+
+def extract_text(soup: BeautifulSoup, join_str: Optional[str] = None) -> str:
+    """Extract the text from a BeautifulSoup object.
+
+    Args:
+        soup: The BeautifulSoup object to extract the text from.
+        join_str: String to join the text parts with. Per default a space is used.
+    Returns:
+        Text from the BeautifulSoup object.
+    """
+    if join_str is None:
+        join_str = " "
+    texts = list(soup.stripped_strings)
+    result: str = join_str.join(texts)
+    return result
+
+
+def extract_one(soup: BeautifulSoup, name=None, attrs: Optional[dict] = None, **kwargs: Dict[str, Any]) -> Any:
+    """Extract exactly one specified element from a BeautifulSoup object.
+
+    This function expacts that exactly only one result is found.
+    Otherwise a RuntimeError is raised.
+
+    Args:
+        soup: The BeautifulSoup object to extract the element from.
+        name: Name of the tag to extract.
+        attrs: Attributes of the tag to extract.
+        kwargs: Additional keyword arguments.
+    Returns:
+        The extracted BeautifulSoup element.
+    Raises:
+        RuntimeError: If not exactly one result is found.
+    """
+    if attrs is None:
+        attrs = {}
+    result = soup.find_all(name, attrs, **kwargs)
+    if len(result) != 1:
+        raise RuntimeError(f"Expected exactly one result, but got {len(result)}!")
+    result = result[0]
+    return result
+
+
+def extract_all(soup: BeautifulSoup, name=None, attrs: Optional[dict] = None, **kwargs: Dict[str, Any]) -> Any:
+    """Extract all specified elements from a BeautifulSoup object.
+
+    Args:
+        soup: The BeautifulSoup object to extract the elements from.
+        name: Name of the tag to extract.
+        attrs: Attributes of the tag to extract.
+        kwargs: Additional keyword arguments.
+    Returns:
+        The extracted BeautifulSoup elements.
+    """
+    if attrs is None:
+        attrs = {}
+    result = soup.find_all(name, attrs, **kwargs)
+    return result
+
+
+def remove_all(soup: BeautifulSoup, name=None, attrs: Optional[dict] = None, **kwargs: Dict[str, Any]) -> None:
+    """Remove all specified elements from a BeautifulSoup object.
+
+    The removal is done in place. Nothing is returned.
+
+    Args:
+        soup: The BeautifulSoup object to remove the elements from.
+        name: Name of the tag(-s) to remove.
+        attrs: Attributes of the tag(-s) to remove.
+        kwargs: Additional keyword arguments.
+    """
+    if attrs is None:
+        attrs = {}
+    result = soup.find_all(name, attrs, **kwargs)
+    for r in result:
+        r.decompose()
+
+
+def soup_to_md(soup: BeautifulSoup, mdformat_options: Optional[dict] = None) -> str:
+    """Convert a BeautifulSoup object to Markdown.
+
+    The default mdformat options are:
+
+    - ``number=True``: apply consecutive numbering to ordered lists
+    - ``wrap="no"``: paragraph word wrap mode
+    - ``end-of-line="lf"``: use LF as line ending
+
+    See Also:
+        The `mdformat Options
+        <https://mdformat.readthedocs.io/en/stable/users/installation_and_usage.html#options>`_.
+    Args:
+        soup: BeautifulSoup object.
+        mdformat_options: Options for mdformat.
+    Returns:
+        The Markdown text.
+    """
+    if mdformat_options is None:
+        mdformat_options = {"number": True, "wrap": "no"}
+    text = MarkdownConverter().convert_soup(soup)
+    text = mdformat.text(text, options=mdformat_options)
+    return text
+
+
+def html_to_md(html: str, mdformat_options: Optional[dict] = None) -> str:
+    """Convert HTML to Markdown.
+
+    The default mdformat options are:
+
+    - ``number=True``: apply consecutive numbering to ordered lists
+    - ``wrap="no"``: paragraph word wrap mode
+    - ``end-of-line="lf"``: use LF as line ending
+
+    See Also:
+        The `mdformat Options
+        <https://mdformat.readthedocs.io/en/stable/users/installation_and_usage.html#options>`_.
+    Args:
+        html: HTML text.
+        mdformat_options: Options for mdformat.
+    Returns:
+        The Markdown text.
+    """
+    if mdformat_options is None:
+        mdformat_options = {"number": True, "wrap": "no"}
+    text = markdownify(html)
+    text = mdformat.text(text, options=mdformat_options)
+    return text
---
+++ tests/test_bs.py
@@ -0,0 +1,91 @@
+# Copyright (c) 2024 Philip May
+# Copyright (c) 2004-2023 Leonard Richardson
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
+import pytest
+from bs4 import BeautifulSoup
+
+from mltb2.bs import extract_all, extract_one, extract_text, html_to_md, remove_all, soup_to_md
+
+# this code snippet is from the BeautifulSoup documentation
+# MIT License
+# Copyright (c) 2004-2023 Leonard Richardson
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>"""
+
+
+@pytest.fixture
+def my_soup() -> BeautifulSoup:
+    soup = BeautifulSoup(html_doc, features="html.parser")
+    return soup
+
+
+@pytest.mark.xfail(reason="see https://github.com/telekom/mltb2/issues/127")
+def test_extract_text(my_soup: BeautifulSoup):
+    result = extract_text(my_soup)
+    assert (
+        result == "The Dormouse's story The Dormouse's story Once upon a time there were three little sisters; "
+        "and their names were Elsie, Lacie and Tillie; and they lived at the bottom of a well. ..."
+    )
+
+
+def test_extract_one__happy_case(my_soup: BeautifulSoup):
+    result = extract_one(my_soup, name="head")
+    assert result is not None
+    assert result.name == "head"
+    assert result.text == "The Dormouse's story"
+
+
+def test_extract_one__multiple_results(my_soup: BeautifulSoup):
+    with pytest.raises(RuntimeError):
+        _ = extract_one(my_soup, name="a")
+
+
+def test_extract_all(my_soup: BeautifulSoup):
+    result = extract_all(my_soup, name="a")
+    assert result is not None
+    assert len(result) == 3
+    assert result[0].name == "a"
+    assert result[0].text == "Elsie"
+
+
+def test_remove_all(my_soup: BeautifulSoup):
+    remove_all(my_soup, name="a")
+    result = extract_all(my_soup, name="a")
+    assert result is not None
+    assert len(result) == 0
+
+
+def test_soup_to_md(my_soup: BeautifulSoup):
+    result = soup_to_md(my_soup)
+    assert result is not None
+    assert (
+        result == "The Dormouse's story **The Dormouse's story**\n\n"
+        "Once upon a time there were three little sisters; "
+        "and their names were [Elsie](http://example.com/elsie), "
+        "[Lacie](http://example.com/lacie) and [Tillie](http://example.com/tillie); "
+        "and they lived at the bottom of a well.\n\n...\n"
+    )
+
+
+def test_html_to_md():
+    result = html_to_md(html_doc)
+    assert result is not None
+    assert (
+        result == "The Dormouse's story **The Dormouse's story**\n\n"
+        "Once upon a time there were three little sisters; "
+        "and their names were [Elsie](http://example.com/elsie), "
+        "[Lacie](http://example.com/lacie) and [Tillie](http://example.com/tillie); "
+        "and they lived at the bottom of a well.\n\n...\n"
+    )
