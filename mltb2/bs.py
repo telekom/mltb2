8cfec645588bdcaaacb469262a4ca917e0fa6c87
PhilipMay <philip@may.la>
2024-01-01 12:12:21 +0100

    improve typing and lint

--- mltb2/bs.py
+++ mltb2/bs.py
@@ -12,40 +12,47 @@ Hint:
 from typing import Optional
 
 import mdformat
+from bs4 import BeautifulSoup
 from markdownify import MarkdownConverter, markdownify
 
 
-def extract_text(soup, join_str=None) -> str:
+def extract_text(soup: BeautifulSoup, join_str=None) -> str:
     """TODO: add docstring."""
     if join_str is None:
         join_str = " "
-    texts = [text for text in soup.stripped_strings]
+    texts = list(soup.stripped_strings)
     result: str = join_str.join(texts)
     return result
 
 
-def extract_one(soup, name=None, attrs={}, **kwargs):
+def extract_one(soup: BeautifulSoup, name=None, attrs=None, **kwargs):
     """TODO: add docstring."""
+    if attrs is None:
+        attrs = {}
     result = soup.find_all(name, attrs, **kwargs)
     assert len(result) == 1, len(result)
     result = result[0]
     return result
 
 
-def extract_all(soup, name=None, attrs={}, **kwargs):
+def extract_all(soup: BeautifulSoup, name=None, attrs=None, **kwargs):
     """TODO: add docstring."""
+    if attrs is None:
+        attrs = {}
     result = soup.find_all(name, attrs, **kwargs)
     return result
 
 
-def remove_all(soup, name=None, attrs={}, **kwargs) -> None:
+def remove_all(soup: BeautifulSoup, name=None, attrs=None, **kwargs) -> None:
     """TODO: add docstring."""
+    if attrs is None:
+        attrs = {}
     result = soup.find_all(name, attrs, **kwargs)
     for r in result:
         r.decompose()
 
 
-def soup_to_md(soup, mdformat_options: Optional[dict] = None) -> str:
+def soup_to_md(soup: BeautifulSoup, mdformat_options: Optional[dict] = None) -> str:
     """TODO: add docstring."""
     if mdformat_options is None:
         mdformat_options = {"number": True, "wrap": "no"}
