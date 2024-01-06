3bec769a3ad09df7a1e4749d040f275e7e7d31e6
PhilipMay <philip@may.la>
2024-01-06 20:50:44 +0100

    Add hypothesis tests for OpenAiTokenCounter class

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -2,8 +2,21 @@
 # This software is distributed under the terms of the MIT license
 # which is available at https://opensource.org/licenses/MIT
 
+from typing import List
+
+from hypothesis import given, settings
+from hypothesis.strategies import lists, text
 
 from mltb2.openai import OpenAiTokenCounter
+
+
+@settings(max_examples=1000)
+@given(text())
+def test_OpenAiTokenCounter_str_hypothesis(text: str):  # noqa: N802
+    token_counter = OpenAiTokenCounter("gpt-4")
+    token_count = token_counter(text)
+
+    assert token_count >= 0
 
 
 def test_OpenAiTokenCounter_call_string():  # noqa: N802
@@ -11,6 +24,15 @@ def test_OpenAiTokenCounter_call_string():  # noqa: N802
     token_count = token_counter("Das ist ein Text.")
 
     assert token_count == 5
+
+
+@settings(max_examples=1000)
+@given(lists(text()))
+def test_OpenAiTokenCounter_list_hypothesis(texts: List[str]):  # noqa: N802
+    token_counter = OpenAiTokenCounter("gpt-4")
+    token_count = token_counter(texts)
+
+    assert len(token_count) == len(texts)
 
 
 def test_OpenAiTokenCounter_call_list():  # noqa: N802
